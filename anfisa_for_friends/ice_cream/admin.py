eaf6e6202a832417477922bd940aad1e503d922f
Denis Savvateev <densavvateev@yandex.ru>
2023-10-11 23:10:02 +0300

    Make fine tuning of admin-zone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,11 +1,47 @@
 from django.contrib import admin
 
 # Register your models here.
-# Из модуля models импортируем модель Category...
+# Из модуля models импортируем модели
 from .models import Category, Wrapper, Topping, IceCream
 
-# ...и регистрируем её в админке:
-admin.site.register(Category)
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',        
+    )
+
+
+# ...и регистрируем их в админке:
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Wrapper)
 admin.site.register(Topping)
-admin.site.register(IceCream)
+admin.site.register(IceCream, IceCreamAdmin)
